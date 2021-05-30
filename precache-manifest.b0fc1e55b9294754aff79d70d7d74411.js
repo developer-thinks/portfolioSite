839a068f6191b54341a53eac359b6147c69e2033
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2021-05-30 16:00:46 +0530

    Updates

---
+++ precache-manifest.b0fc1e55b9294754aff79d70d7d74411.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3275e6ab334567f0b6d8ddfc6ec17548",
+    "url": "/interactive-resume/index.html"
+  },
+  {
+    "revision": "264fbcf48544db8ff558",
+    "url": "/interactive-resume/static/css/2.956e9ece.chunk.css"
+  },
+  {
+    "revision": "b09bea16c8d052ed9719",
+    "url": "/interactive-resume/static/css/main.225620a0.chunk.css"
+  },
+  {
+    "revision": "264fbcf48544db8ff558",
+    "url": "/interactive-resume/static/js/2.b6507041.chunk.js"
+  },
+  {
+    "revision": "3a439d0148361576cf719017f69da164",
+    "url": "/interactive-resume/static/js/2.b6507041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09bea16c8d052ed9719",
+    "url": "/interactive-resume/static/js/main.83f0ca8c.chunk.js"
+  },
+  {
+    "revision": "e0a69dba9960e7a7e634",
+    "url": "/interactive-resume/static/js/runtime-main.920387d7.js"
+  },
+  {
+    "revision": "5ed2b0a406fe49df0cbfe1b180d5d667",
+    "url": "/interactive-resume/static/media/ali-proflie.5ed2b0a4.jpg"
+  },
+  {
+    "revision": "dd209bf0d4d7bef7baa4569e49e2e17b",
+    "url": "/interactive-resume/static/media/pathfinding.dd209bf0.png"
+  },
+  {
+    "revision": "4dac4ace946eb482a7cb65bbcf2bdc9c",
+    "url": "/interactive-resume/static/media/sorting.4dac4ace.png"
+  }
+]);
