5befe535592d29cbc0777354cab7acfeae246d25
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2021-05-31 19:51:59 +0530

    Updates

---
+++ precache-manifest.7a52d336f0842ab176652c1314b3d262.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e411efc96a56b50c4f0ffe81914c29c",
+    "url": "/interactive-resume/index.html"
+  },
+  {
+    "revision": "e0543e4ad0aea2b379b0",
+    "url": "/interactive-resume/static/css/2.956e9ece.chunk.css"
+  },
+  {
+    "revision": "9c1c39fab3ecb60cfafc",
+    "url": "/interactive-resume/static/css/main.31973339.chunk.css"
+  },
+  {
+    "revision": "e0543e4ad0aea2b379b0",
+    "url": "/interactive-resume/static/js/2.2dcffe30.chunk.js"
+  },
+  {
+    "revision": "3a439d0148361576cf719017f69da164",
+    "url": "/interactive-resume/static/js/2.2dcffe30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c1c39fab3ecb60cfafc",
+    "url": "/interactive-resume/static/js/main.e6cd53d9.chunk.js"
+  },
+  {
+    "revision": "e0a69dba9960e7a7e634",
+    "url": "/interactive-resume/static/js/runtime-main.920387d7.js"
+  },
+  {
+    "revision": "5ed2b0a406fe49df0cbfe1b180d5d667",
+    "url": "/interactive-resume/static/media/ali-proflie.5ed2b0a4.jpg"
+  },
+  {
+    "revision": "dd209bf0d4d7bef7baa4569e49e2e17b",
+    "url": "/interactive-resume/static/media/pathfinding.dd209bf0.png"
+  },
+  {
+    "revision": "4dac4ace946eb482a7cb65bbcf2bdc9c",
+    "url": "/interactive-resume/static/media/sorting.4dac4ace.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/interactive-resume/precache-manifest.04ae8371483af7ea9b25156cc32f15e5.js"
+  "/interactive-resume/precache-manifest.7a52d336f0842ab176652c1314b3d262.js"
 );
 
 self.addEventListener('message', (event) => {
